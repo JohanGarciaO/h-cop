9e9a9ca2819389c67be46ece7b98ef6fb7eb515e
3S JOHAN <johangarciadeoliveira123@outlook.com.br>
2025-06-27 12:20:50 -0300

    refactor: add `NEEDS_MAINTENANCE` to `RoomCleaningStatus Enum`

--- src/app/Enums/RoomCleaningStatus.php
+++ src/app/Enums/RoomCleaningStatus.php
@@ -9,12 +9,14 @@ enum RoomCleaningStatus
 
     case READY;
     case IN_PREPARATION;
+    case NEEDS_MAINTENANCE;
 
     public function label(): string
     {
         return match ($this) {
             SELF::READY => 'pronto para uso',
             SELF::IN_PREPARATION => 'em preparo',
+            SELF::NEEDS_MAINTENANCE => 'em manutenção',
         };
     }
 
